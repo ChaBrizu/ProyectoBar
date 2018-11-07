a8e9bd1a9d05cad1f7efddec5cbf56c6782be784
ChaBrizu <carlos.chabrizu@gmail.com>
2018-11-07 16:49:08 -0600

    Añadido el servicio web
    .

---
+++ WebServiceBar/src/java/WSBar/WSBar.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package WSBar;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import BaseDeDatos.*;
+
+/**
+ *
+ * @author Carlos
+ */
+@WebService(serviceName = "WSBar")
+public class WSBar {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "insert")
+    public String insert(@WebParam(name = "strTabla") String strTabla, @WebParam(name = "strDatos") String strDatos) {
+        InterfazBD obj;
+        obj= new InsertBD(strTabla, strDatos);
+        return obj.consultaBd();
+    }
+    
+    @WebMethod(operationName = "update")
+    public String update(@WebParam(name = "strTabla") String strTabla, @WebParam(name = "strDatos") String strDatos, @WebParam(name = "strCondicion") String strCondicion) {
+        InterfazBD obj;
+        obj= new UpdateBD(strTabla, strDatos, strCondicion);
+        return obj.consultaBd();
+    }
+    
+    @WebMethod(operationName = "delete")
+    public String delete(@WebParam(name = "strTabla") String strTabla, @WebParam(name = "strDatos") String strDatos, @WebParam(name = "strCondicion") String strCondicion) {
+        InterfazBD obj;
+        obj= new DeleteBD(strTabla, strCondicion);
+        return obj.consultaBd();
+    }
+}
