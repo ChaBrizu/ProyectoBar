a09c0d5f89652abe96348d071fc503f0f2a087c6
ChaBrizu <carlos.chabrizu@gmail.com>
2018-12-11 01:22:15 -0600

    WSMonkysREST añadido
    El Web service es ahora RESTful, por lo que envía los datos en forma de JSON. En teoría, debe funcionar
    lo logré jaja salu2

---
+++ WSMonkysREST/src/java/MonkysBar/Productos.java
@@ -0,0 +1,143 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MonkysBar;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Carlos
+ */
+@Entity
+@Table(name = "productos")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Productos.findAll", query = "SELECT p FROM Productos p")
+    , @NamedQuery(name = "Productos.findByIdProducto", query = "SELECT p FROM Productos p WHERE p.idProducto = :idProducto")
+    , @NamedQuery(name = "Productos.findByNomProducto", query = "SELECT p FROM Productos p WHERE p.nomProducto = :nomProducto")
+    , @NamedQuery(name = "Productos.findByCantidad", query = "SELECT p FROM Productos p WHERE p.cantidad = :cantidad")
+    , @NamedQuery(name = "Productos.findByPrecio", query = "SELECT p FROM Productos p WHERE p.precio = :precio")
+    , @NamedQuery(name = "Productos.findByDescripcion", query = "SELECT p FROM Productos p WHERE p.descripcion = :descripcion")})
+public class Productos implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_producto")
+    private Integer idProducto;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 30)
+    @Column(name = "nom_producto")
+    private String nomProducto;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "cantidad")
+    private int cantidad;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "precio")
+    private float precio;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    public Productos() {
+    }
+
+    public Productos(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public Productos(Integer idProducto, String nomProducto, int cantidad, float precio, String descripcion) {
+        this.idProducto = idProducto;
+        this.nomProducto = nomProducto;
+        this.cantidad = cantidad;
+        this.precio = precio;
+        this.descripcion = descripcion;
+    }
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNomProducto() {
+        return nomProducto;
+    }
+
+    public void setNomProducto(String nomProducto) {
+        this.nomProducto = nomProducto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idProducto != null ? idProducto.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Productos)) {
+            return false;
+        }
+        Productos other = (Productos) object;
+        if ((this.idProducto == null && other.idProducto != null) || (this.idProducto != null && !this.idProducto.equals(other.idProducto))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "MonkysBar.Productos[ idProducto=" + idProducto + " ]";
+    }
+    
+}
