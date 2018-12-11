f6cf03f442ca1681d16dae9eef7b23b982916a2b
nibtlesz <emmanuel_ostria@hotmail.com>
2018-12-11 16:10:21 -0600

    Ya puedes descargarlo Ian

---
+++ PuntoVentas/Punto de ventas/ModelsClass/TextBoxEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto_de_ventas.ModelsClass
+{
+    public class TextBoxEvent
+    {
+    }
+}
